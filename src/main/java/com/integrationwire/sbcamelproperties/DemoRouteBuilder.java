67f3ba83fec1cd54be94bcc5de212ae2f124a8ff
Tom Donohue <tom@monodot.co.uk>
2017-08-16 21:42:05 +0100

    Add route and config

--- src/main/java/com/integrationwire/sbcamelproperties/DemoRouteBuilder.java
+++ src/main/java/com/integrationwire/sbcamelproperties/DemoRouteBuilder.java
@@ -1,20 +1,19 @@
+package com.integrationwire.sbcamelproperties;
+
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 @Component
 public class DemoRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() {
-        from("timer:trigger")
-                .transform().simple("ref:myBean")
-                .to("log:out");
-    }
+        from("timer:mytimer?period={{timer.period}}")
+            .setBody(simple("{{greeting.word}}, this is Camel running in Spring Boot!"))
+            .to("log:out");
 
-    @Bean
-    String myBean() {
-        return "I'm Spring bean!";
     }
 
 }
